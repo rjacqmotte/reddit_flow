d13245c0633a5a2c18511ccc7a66d23b3a6c49af
rjacqmotte <rjacqmotte@gmail.com>
2026-04-10 20:27:54 +0200

    create logic to detect overflow in card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,41 @@
 import styles from './Card.module.css';
+import { useRef, useEffect, useState } from 'react';
 
-export const Card = ({ title, body, imageUrl }) => {
-    return (
-        <div className={styles.cardContainer}>
-            <h2>{title}</h2>
-            { imageUrl && (
-                <div className={styles.cardImageContainer}>
-                <img 
-                    className={styles.cardImage}
-                    src={imageUrl}
-                    alt='article image'/>
-            </div>
-            )}
-            <p>{body}</p>
-            <div className={styles.cardExpendZone}>
-                {/* <button className={styles.cardExpendButton}/> */}
-            </div>
-        </div>
-    )
+export const Card = ({ id, title, body, imageUrl }) => {
+  const contentRef = useRef(null);
+  const viewRef = useRef(null);
+  const [hasOverFlow, setHasOverFlow] = useState(false);
+
+  useEffect(() => {
+    const content = contentRef.current;
+    const view = viewRef.current;
+    console.log(content.scrollHeight);
+    console.log(view.clientHeight);
+    if (content && view) {
+      setHasOverFlow(content.scrollHeight > view.clientHeight);
+    }
+  }, [title, body, imageUrl]);
+
+  return (
+    <div ref={viewRef} className={styles.cardContainer}>
+      <div ref={contentRef} className={styles.cardContentContainer}>
+        <h2>{title}</h2>
+        {imageUrl && (
+          <div className={styles.cardImageContainer}>
+            <img
+              className={styles.cardImage}
+              src={imageUrl}
+              alt="article image"
+            />
+          </div>
+        )}
+        <p>{body}</p>
+        {hasOverFlow && (
+          <div className={styles.cardExpendZone}>
+            {/* <button className={styles.cardExpendButton}/> */}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
