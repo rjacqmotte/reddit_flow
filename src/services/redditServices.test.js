51cacc0bafb684df533cd73037f76d7858b78d92
rjacqmotte <rjacqmotte@gmail.com>
2026-04-21 13:58:37 +0200

    test(fecthPopular): create fectch.popular.test.js

---
+++ src/services/redditServices.test.js
@@ -0,0 +1,7 @@
+import { fetchPopular } from "./redditServices";
+
+describe('fetchPopular', () => {
+    it('returns an array called data', () => {});
+    it('manages http error', () => {});
+    it('manages reddit error', () => {});
+});
