b11fe510b7f1a5f7b0015c6925fad83ee2e1dbac
rjacqmotte <rjacqmotte@gmail.com>
2026-04-29 11:32:17 +0200

    feat(articles): fecth reddit api articles

--- src/features/articles/Articles.jsx
+++ src/features/articles/Articles.jsx
@@ -1,14 +1,23 @@
 /** Display the flow of articles to scroll. default page. */
+import { useEffect, useState } from 'react';
+import { fetchPopular } from '../../services/redditServices';
 import { Card } from '../../components/Card/Card';
 import styles from './articles.module.css';
 
-export const Articles =  ({ articles }) => {
-
+export const Articles =  () => {
+    const [articles, setArticles] = useState([]);
+    useEffect(() => {
+        const fetchArticles = async () => {
+            const articles = await fetchPopular();
+            setArticles(articles);
+        };
+        fetchArticles();
+    }, []);
 
     return (
         <div className={styles.articlesContainer}>
             <ul className={styles.ArticlesList}>
-                {articles &&articles.map((article, index) => (
+                {articles && articles.map((article, index) => (
                     <li key={index} className={styles.ArticleItem}>
                         <Card article = {article.data} />
                     </li>
