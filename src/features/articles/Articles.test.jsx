ecfa5356970b48a81cace2e5057fe8de9b9c5c69
rjacqmotte <rjacqmotte@gmail.com>
2026-05-03 21:58:53 +0200

    test(Articles): fix test unit to manage fecth in useEffect

--- src/features/articles/Articles.test.jsx
+++ src/features/articles/Articles.test.jsx
@@ -1,26 +1,43 @@
 import { Articles } from './Articles';
 import { mockRealRedditArticles } from '../../mock/mockRealRedditArticles';
 import { render, screen } from '@testing-library/react';
+import * as redditServices from '../../services/redditServices';
 
-describe('Articles', () => {
+
 const mockArticles = mockRealRedditArticles.data.children;
 
-  it('renders one card per article', () => {
-    render(<Articles articles={mockArticles} />);
-    expect(screen.getAllByRole('heading')).toHaveLength(
-      Object.values(mockArticles).length
-    );
+afterEach(() => {
+  vi.restoreAllMocks();
+});
+
+describe('Articles', () => {
+  // test asynchrone car c'est un useEffect qui fecth les données,
+  // utilisation de async dans le it.
+  it('renders one card per article', async () => {
+    // utlisation de .spyOn : voir doc vitest_mock_fetch_synthese
+    vi.spyOn(redditServices, 'fetchPopular').mockResolvedValue(mockArticles);
+
+    render(<Articles />);
+    // attendre que les données soient cherchée et arrivent
+    const headings = await screen.findAllByRole('heading');
+    expect(headings).toHaveLength(mockArticles.length);
   });
-  it('renders the title of every article', () => {
-    render(<Articles articles={mockArticles} />);
-    mockArticles.map((article) => {
-      expect(screen.getByText(article.data.title)).toBeInTheDocument();
-    });
+
+  it('renders the title of every article', async () => {
+    vi.spyOn(redditServices, 'fetchPopular').mockResolvedValue(mockArticles);
+    
+    render(<Articles />);
+
+    for (const article of mockArticles) {
+      expect(await screen.findByText(article.data.title)).toBeInTheDocument();
+    };
   });
+
   it('renders nothing if articles is null', () => {
     render(<Articles articles={null} />);
     expect(screen.queryByRole('heading')).not.toBeInTheDocument();
   });
+
   it('renders nothing if articles is empty', () => {
     render(<Articles articles={[]} />);
     expect(screen.queryByRole('heading')).not.toBeInTheDocument();
