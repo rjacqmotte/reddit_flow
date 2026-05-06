99489f08bd36b2e63624da334a6f09aeb0589317
renaud <rjacqmotte@gmail.com>
2026-05-06 22:07:12 +0200

    test(Articles): check message error if error

--- src/features/articles/Articles.jsx
+++ src/features/articles/Articles.jsx
@@ -6,23 +6,30 @@ import styles from './articles.module.css';
 
 export const Articles =  () => {
     const [articles, setArticles] = useState([]);
+    const [fetchError, setFetchError] = useState(false);
     useEffect(() => {
         const fetchArticles = async () => {
+            try {
             const articles = await fetchPopular();
             setArticles(articles);
+            } catch(error) {
+                console.error(error.message);
+                setFetchError(true);
+            };
         };
         fetchArticles();
     }, []);
 
     return (
         <div className={styles.articlesContainer}>
-            <ul className={styles.ArticlesList}>
+            { fetchError ? <h2>Error - Something get wrong : /</h2> : (<ul className={styles.ArticlesList}>
                 {articles && articles.map((article, index) => (
                     <li key={index} className={styles.ArticleItem}>
                         <Card article = {article.data} />
                     </li>
                 ))}
-            </ul>
+            </ul>)
+            }
         </div>
     );
 };
